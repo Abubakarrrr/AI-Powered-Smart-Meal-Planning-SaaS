27ec4ffddaf2c65f16a8cfe5c50616a1017e6883
Abubakar <abubakkeramjad10@gmail.com>
2025-02-18 13:10:28 +0500

    resolced tsconfig issue in backend

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI || "";
 
-
-
-const connectDB = async (MONGO_URI:any) => {
+const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
     console.log(" MongoDB Connected");
--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db";
+import connectDB from "@config/db";
 dotenv.config();
 
-dotenv.config({path:"./.env"});
-
-const MONGO_URI = process.env.MONGO_URI || "";
-// Initialize Express
 const app = express();
+connectDB();
 
-// Connect to MongoDB
-connectDB(MONGO_URI);
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
