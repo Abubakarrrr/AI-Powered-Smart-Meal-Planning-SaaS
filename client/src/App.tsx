aacd67dbd7538f47e26e9c988256cf6e314fa1f7
Abubakar <abubakkeramjad10@gmail.com>
2025-02-18 19:27:46 +0500

    added login signup forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css'
-import Button from '@components/Button'
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Signup from "Forms/Signup";
 
+import "./index.css";
+import Login from "Forms/Login";
+function App() {
   return (
     <>
-     <h1>hello</h1>
-     <Button/>
+      <div className="font-primary">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
